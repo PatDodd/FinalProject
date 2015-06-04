aa492d262b85ccd9927fcc29570e5998393dc212
PatDodd <pdodd001@seattlecentral.edu>
2015-06-03 21:03:54 -0700

    Saving state of application before adding sqlite

--- server/handlers/home.js
+++ server/handlers/home.js
@@ -2,10 +2,16 @@
 var fs = require('fs');
 
 module.exports = function(req, reply){
+
   fs.readFile("models/library.json", "utf8", function(err, data){
+    var albums = JSON.parse(data).albums;
+
     reply.view("index", {
         title: "Home",
-        albums: JSON.parse(data).albums
+        albums: albums
+        // image: album.albumArtMed.substring(0,33),
+        // artist: album.artist.substring(0,33),
+        // albumName: album.albumName.substring(0,33)
     });
   });
 };
